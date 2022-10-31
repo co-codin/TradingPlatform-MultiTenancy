ab8cf1beb1b696b99284c5c6e33f71e8aefd8f38
Yeqing <cuiyeqing960904@gmail.com>
2022-10-31 09:06:54 -0400

    worker db structure built

--- Modules/Worker/Database/Migrations/2022_10_31_124756_create_workers_table.php
+++ Modules/Worker/Database/Migrations/2022_10_31_124756_create_workers_table.php
@@ -16,12 +16,14 @@ return new class extends Migration
         Schema::create('workers', function (Blueprint $table) {
             $table->id();
             $table->string('username');
-            $table->string('first_name')
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->softDeletes();
-
+            $table->timestamp('last_login')->nullable();
             $table->timestamps();
         });
     }
