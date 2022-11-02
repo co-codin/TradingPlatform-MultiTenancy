985f7dd4e6caf3b159c9856da3ae96bbe6583284
Yeqing <cuiyeqing960904@gmail.com>
2022-11-02 05:26:11 -0400

    worker factory

--- Modules/Worker/Database/factories/WorkerFactory.php
+++ Modules/Worker/Database/factories/WorkerFactory.php
@@ -3,6 +3,7 @@
 namespace Modules\Worker\Database\factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class WorkerFactory extends Factory
 {
@@ -21,7 +22,13 @@ class WorkerFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'username' => $this->faker->userName,
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'password' => bcrypt('admin'),
+            'remember_token' => Str::random(10),
+            'is_active' => true,
         ];
     }
 }
