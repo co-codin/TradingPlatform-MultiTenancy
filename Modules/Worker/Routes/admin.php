0fedec2399c0f56ab792db45bcb8dcdc7bd59d03
Yeqing <cuiyeqing960904@gmail.com>
2022-11-07 09:55:34 -0500

    merging

--- Modules/Worker/Routes/admin.php
+++ Modules/Worker/Routes/admin.php
@@ -5,3 +5,4 @@ use Modules\Worker\Http\Controllers\Admin\Desk\WorkerDeskController;
 
 # Desk
 Route::put('workers/{worker}/desk', [WorkerDeskController::class, 'update'])->name('worker.desk.update');
+
