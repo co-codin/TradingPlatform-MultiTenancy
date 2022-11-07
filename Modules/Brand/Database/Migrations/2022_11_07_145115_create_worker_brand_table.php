cc3076175abfb3bdfe83ef77e729e1cce2ea70a9
Yeqing <cuiyeqing960904@gmail.com>
2022-11-07 09:52:31 -0500

    worker brand table

---
+++ Modules/Brand/Database/Migrations/2022_11_07_145115_create_worker_brand_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('worker_brand', function (Blueprint $table) {
+            $table->id();
+            $table->primary(['worker_id', 'brand_id'], 'id');
+            $table->foreignId('worker_id')->constrained();
+            $table->foreignId('brand_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('worker_brand');
+    }
+};
