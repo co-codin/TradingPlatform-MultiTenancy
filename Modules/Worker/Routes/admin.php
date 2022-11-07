8c0acb5385f019f863dd35eea349339ee010a821
Yeqing <cuiyeqing960904@gmail.com>
2022-11-07 09:59:45 -0500

    worker brand built

--- Modules/Worker/Routes/admin.php
+++ Modules/Worker/Routes/admin.php
@@ -4,9 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Modules\Worker\Http\Controllers\Admin\AuthController;
 use Modules\Worker\Http\Controllers\Admin\Desk\WorkerDeskController;
 use Modules\Worker\Http\Controllers\Admin\ForgetController;
+use Modules\Worker\Http\Controllers\Admin\Brand\WorkerBrandController;
 
 # Desk
 Route::put('workers/{worker}/desk', [WorkerDeskController::class, 'update'])->name('worker.desk.update');
+
+# Brand
+Route::put('workers/{worker}/brand', [WorkerBrandController::class, 'update'])->name('worker.brand.update');
 
 # Auth
 Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
