9c2ae219bf9483f8f2c7d5aaf99c5cae41440ee7
Yeqing <cuiyeqing960904@gmail.com>
2022-11-07 09:48:01 -0500

    brand crud built

--- Modules/Brand/Http/Controllers/Admin/BrandController.php
+++ Modules/Brand/Http/Controllers/Admin/BrandController.php
@@ -3,6 +3,7 @@
 namespace Modules\Brand\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Modules\Brand\Dto\BrandDto;
 use Modules\Brand\Http\Requests\BrandCreateRequest;
 use Modules\Brand\Http\Requests\BrandUpdateRequest;
 use Modules\Brand\Http\Resources\BrandResource;
@@ -45,16 +46,30 @@ class BrandController extends Controller
 
     public function store(BrandCreateRequest $request)
     {
+        $brandDto = BrandDto::fromFormRequest($request);
 
+        $brand = $this->brandStorage->store($brandDto);
+
+        return new BrandResource($brand);
     }
 
     public function update(int $brand, BrandUpdateRequest $request)
     {
+        $brand = $this->brandRepository->find($brand);
 
+        $brand = $this->brandStorage->update(
+            $brand, BrandDto::fromFormRequest($request)
+        );
+
+        return new BrandResource($brand);
     }
 
     public function destroy(int $brand)
     {
+        $brand = $this->brandRepository->find($brand);
 
+        $this->brandStorage->delete($brand);
+
+        return response()->noContent();
     }
 }
--- Modules/Brand/Services/BrandStorage.php
+++ Modules/Brand/Services/BrandStorage.php
@@ -2,20 +2,31 @@
 
 namespace Modules\Brand\Services;
 
+use Modules\Brand\Dto\BrandDto;
+use Modules\Brand\Models\Brand;
+
 class BrandStorage
 {
-    public function store()
+    public function store(BrandDto $brandDto)
     {
-
+        return Brand::query()->create($brandDto->toArray());
     }
 
-    public function update()
+    public function update(Brand $brand, BrandDto $brandDto)
     {
+        $attributes = $brandDto->toArray();
 
+        if (!$brand->update($attributes)) {
+            throw new \LogicException('can not update brand.');
+        }
+
+        return $brand;
     }
 
-    public function delete()
+    public function delete(Brand $brand)
     {
-
+        if (!$brand->delete()) {
+            throw new \LogicException('can not delete brand');
+        }
     }
 }
