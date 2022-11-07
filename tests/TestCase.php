7845d0ba53d6c496c7111ab5e54d7b9e2b8b299f
Yeqing <cuiyeqing960904@gmail.com>
2022-11-07 10:42:41 -0500

    user + admin global auth added

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,10 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use Modules\Role\Enums\DefaultRole;
+use Modules\Role\Models\Role;
+use Modules\Worker\Models\Worker;
 use Spatie\Permission\Traits\RefreshesPermissionCache;
 
 abstract class TestCase extends BaseTestCase
@@ -13,10 +17,41 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
+        Artisan::call('optimize');
     }
 
     protected function authenticateWorker()
     {
+        Worker::factory()->create([
+            'email' => 'admin@admin.ru'
+        ]);
 
+        $response = $this->json('POST', route('admin.auth.login'), [
+            'email' => 'admin@admin.ru',
+            'password' => 'admin',
+        ]);
+
+        $this->withToken($response->json('token'));
+    }
+
+    protected function authenticateAdmin()
+    {
+        $worker = Worker::factory()->create([
+            'email' => 'admin@admin.ru'
+        ]);
+
+        $role = Role::factory()->create([
+            'name' => DefaultRole::ADMIN
+        ]);
+
+        $worker->roles()->sync($role);
+
+        $response = $this->json('POST', route('admin.auth.login'), [
+            'email' => 'admin@admin.ru',
+            'password' => 'admin',
+        ]);
+
+        $this->withToken($response->json('token'));
     }
 }
+
