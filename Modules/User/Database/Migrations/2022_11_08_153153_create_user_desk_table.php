d00ed5a0de5cde59a72b8758619d3ad67d84bdf0
Yeqing <cuiyeqing960904@gmail.com>
2022-11-08 10:32:47 -0500

    user desk

---
+++ Modules/User/Database/Migrations/2022_11_08_153153_create_user_desk_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_desk', function (Blueprint $table) {
+            $table->primary(['user_id', 'desk_id'], 'id');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('desk_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_desk');
+    }
+};
