d2c95e8f8b684832c9a7ee9f0058aa1d98cffa1c
Roman Morgunov <roman.m@stoxtech.dev>
2022-11-09 14:35:55 +0400

    Added JsonMiddleware to auto convert responses to JSON format

---
+++ app/Http/Middleware/JsonMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+final class JsonMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure(Request): (Response|RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+
+        return $next($request);
+    }
+}
