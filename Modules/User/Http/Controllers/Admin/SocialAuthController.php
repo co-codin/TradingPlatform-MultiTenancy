c7c74f02ad2fa01a37564574abcb6e58ff1e070d
a.myslin <a.myslin@chronopay.com>
2022-11-10 12:33:04 +0500

    STX-10:upd: Oauth implementation

--- Modules/User/Http/Controllers/Admin/SocialAuthController.php
+++ Modules/User/Http/Controllers/Admin/SocialAuthController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Modules\User\Http\Controllers\Admin;
 
+use OpenApi\Annotations as OA;
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use Modules\User\Http\Resources\AuthUserResource;
@@ -37,6 +38,33 @@ class SocialAuthController extends Controller
         ];
     }
 
+    /**
+     * @OA\Get(
+     *     path="/admin/auth/login/{provider}",
+     *     tags={"Auth"},
+     *     summary="OAuth provider login endpoint",
+     *     @OA\Parameter(
+     *          name="provider",
+     *          in="path",
+     *          description="OAuth provider name",
+     *          @OA\Schema (
+     *              type="string",
+     *              example="google"
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=302,
+     *          description="Redirects to Google auth"
+     *     ),
+     *     @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *     )
+     * )
+     *
+     * @param string $provider
+     * @return RedirectResponse
+     */
     public function redirect(string $provider): RedirectResponse
     {
         return Socialite::driver($provider)->redirect();
