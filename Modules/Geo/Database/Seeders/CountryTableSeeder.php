ada0bfe7437fbee7fa66fc0c771e08d52236cfc5
Yeqing <cuiyeqing960904@gmail.com>
2022-11-14 03:40:05 -0500

    country seeder built

--- Modules/Geo/Database/Seeders/CountryTableSeeder.php
+++ Modules/Geo/Database/Seeders/CountryTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\Geo\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Geo\Models\Country;
 
 class CountryTableSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class CountryTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        $rows = json_decode(file_get_contents('Modules/Geo/Database/data/country.json'), true);
+
+        $rows = array_filter($rows, fn($value) => !is_null($value['iso3']));
+
+        Country::query()->truncate();
+
+        foreach ($rows as $row) {
+            Country::query()->create([
+                'name' => $row['name'],
+                'iso2' => $row['iso2'],
+                'iso3' => $row['iso3'],
+            ]);
+        }
     }
 }
