3e5c53b947ad6d78e8a917c51f7c603d20bb6aed
Denis <dotahero.nirvana@gmail.com>
2022-11-15 13:21:01 +0300

    STX-5: создать модуль Language

--- Modules/Language/Models/Language.php
+++ Modules/Language/Models/Language.php
@@ -1,18 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Language\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Language\Database\factories\LanguageFactory;
 
 class Language extends Model
 {
     use HasFactory;
 
+    /**
+     * {@inheritdoc}
+     */
     protected $guarded = ['id'];
 
-    protected static function newFactory()
+    /**
+     * {@inheritDoc}
+     */
+    protected static function newFactory(): LanguageFactory
     {
         return LanguageFactory::new();
     }
