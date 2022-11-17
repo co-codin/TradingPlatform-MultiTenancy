b1a77d3d9965de516b241d3103696ab01cffaba0
Yeqing <cuiyeqing960904@gmail.com>
2022-11-17 08:01:15 -0500

    telescope env

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Laravel\Telescope\EntryType;
+use Illuminate\Support\Collection;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
@@ -20,16 +22,17 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 
         $this->hideSensitiveRequestDetails();
 
-        Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local')) {
-                return true;
+//        $this->filterEntries();
+
+        $this->filterBatches();
+
+        Telescope::tag(function (IncomingEntry $entry) {
+
+            if($entry->type === EntryType::REQUEST) {
+                return ['status:' . $entry->content['response_status']];
             }
 
-            return $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
+            return [];
         });
     }
 
@@ -53,15 +56,56 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         ]);
     }
 
-    /**
-     * Register the Telescope gate.
-     *
-     * This gate determines who can access Telescope in non-local environments.
-     *
-     * @return void
-     */
     protected function gate()
     {
         Gate::define('viewTelescope', fn($user) => true);
+    }
+
+    protected function filterEntries()
+    {
+        Telescope::filter(function (IncomingEntry $entry) {
+            if ($this->app->environment('local')) {
+                return true;
+            }
+
+            return $this->isLoggableRequest($entry) ||
+                $entry->isReportableException() ||
+                $entry->isFailedRequest() ||
+                $entry->isFailedJob() ||
+                $entry->isScheduledTask() ||
+                $entry->hasMonitoredTag();
+        });
+    }
+
+    protected function isLoggableRequest(IncomingEntry $entry) : bool
+    {
+        if($entry->type !== EntryType::REQUEST) {
+            return false;
+        }
+
+        $route = request()->route();
+
+        return in_array(request()->method(), ['POST', 'PUT', 'PATCH', 'DELETE'])
+            || ($entry->content['response_status'] ?? null) == 404;
+    }
+
+    protected function  filterBatches()
+    {
+        Telescope::filterBatch(function (Collection $entries) {
+            if ($this->app->environment('local')) {
+                return true;
+            }
+
+            return $entries->contains(function (IncomingEntry $entry) {
+                return $this->isLoggableRequest($entry) ||
+                    $entry->isReportableException() ||
+                    $entry->isFailedRequest() ||
+                    $entry->isFailedJob() ||
+                    $entry->isScheduledTask() ||
+                    $entry->type === EntryType::MAIL ||
+                    $entry->type === EntryType::NOTIFICATION ||
+                    $entry->hasMonitoredTag();
+            });
+        });
     }
 }
