654af46b370b37de8d8fe69d7f6e5d47af267f9e
Roman Morgunov <roman.m@stoxtech.dev>
2022-11-17 12:39:59 +0400

    Added remember_me parameter for authentication

---
+++ Modules/User/Http/Requests/LoginAuthRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class LoginAuthRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean',
+        ];
+    }
+}
---
+++ tests/Feature/Auth/GetCurrentUserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Auth;
+
+use Tests\TestCase;
+
+final class GetCurrentUserTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function me_endpoint(): void
+    {
+        $this->authenticateUser();
+
+        $response = $this->get(route('admin.auth.user'));
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'first_name',
+                'last_name',
+                'email',
+                'permissions',
+                'role',
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function failed_me_endpoint(): void
+    {
+        $response = $this->get(route('admin.auth.user'));
+
+        $response->assertUnauthorized();
+    }
+}
