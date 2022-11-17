efff7cfc93c880713a302b3a30115f46e4751774
Yeqing <cuiyeqing960904@gmail.com>
2022-11-17 06:27:53 -0500

    updated logics

--- Modules/Brand/Jobs/MigrateDataJob.php
+++ Modules/Brand/Jobs/MigrateDataJob.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Modules\Brand\Models\Brand;
+use Modules\User\Models\User;
 
 class MigrateDataJob implements ShouldQueue
 {
@@ -25,6 +27,15 @@ class MigrateDataJob implements ShouldQueue
     {
         $users = auth('sanctum')->user()->ancestors()->merge(auth('sanctum')->user()->descendants());
 
+//        $userIds = auth('sanctum')->user()->brands()->pluck('pivot.user_id'); // just logics
+
+//        $brandUsers = User::query()->whereIn('id', $userIds)->get();
+
+
+//        $users = unique($users and $brandusers)
+
 //        DB::connection($this->db)->insert($users);
+
+        // такая же логика по другим таблицам 
     }
 }
