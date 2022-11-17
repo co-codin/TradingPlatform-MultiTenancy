10c0cb451dcdd151178ca61ed60e07ab96202abd
Yeqing <cuiyeqing960904@gmail.com>
2022-11-17 05:04:05 -0500

    migrations refactored

--- Modules/Brand/Enums/AllowedDBTables.php
+++ Modules/Brand/Enums/AllowedDBTables.php
@@ -6,17 +6,15 @@ use App\Enums\BaseEnum;
 
 class AllowedDBTables extends BaseEnum
 {
-//    public const USER_LANGUAGE = 'user_language';
+    public const USER_LANGUAGE = 'user_language';
 
-//    public const USER_DEPARTMENT = 'user_department';
+    public const USER_DEPARTMENT = 'user_department';
 
-//    public const USER_DESK = 'user_desk';
+    public const USER_DESK = 'user_desk';
 
     public const USER_COUNTRY = 'user_country';
 
-    public const MODEL_HAS_ROLES = 'model_has_roles';
-
-    public const MODEL_HAS_PERMISSIONS = 'model_has_permissions';
+    public const PERMISSIONS = 'permissions';
 
     public const SALE_STATUSES = 'sale_statuses';
 
