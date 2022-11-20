9aafb46faae2ffa3f44c8dd35bfcb403ebcf3681
Yeqing <cuiyeqing960904@gmail.com>
2022-11-20 07:04:45 -0500

    config structure built

--- Modules/Config/Routes/admin.php
+++ Modules/Config/Routes/admin.php
@@ -1,4 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Config\Http\Controllers\Admin\ConfigTypeController;
+use Modules\Config\Http\Controllers\Admin\ConfigController;
 
+Route::apiResource('/config-types', ConfigTypeController::class);
+Route::apiResource('/configs', ConfigController::class);
