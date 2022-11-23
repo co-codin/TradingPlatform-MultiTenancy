0e3eb7186a85b80b79a7756c9ed8cdc8d82e4f8b
Yeqing <cuiyeqing960904@gmail.com>
2022-11-23 03:38:30 -0500

    module

---
+++ Modules/Customer/Providers/RouteServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Modules\Customer\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The module namespace to assume when generating URLs to actions.
+     *
+     * @var string
+     */
+    protected $moduleNamespace = 'Modules\Customer\Http\Controllers';
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapAdminRoutes();
+    }
+
+
+    protected function mapAdminRoutes()
+    {
+        Route::middleware(['api', 'auth:sanctum'])
+            ->as('admin.')
+            ->prefix('admin')
+            ->group(module_path('Customer', '/Routes/admin.php'));
+    }
+}
