44ce20586228254c9f1e30d1ab36887f4320c8f0
Denis <dotahero.nirvana@gmail.com>
2022-11-23 14:14:50 +0300

    STX-103: Описание всех endpoints Brand в Swagger

--- Modules/Config/Repositories/ConfigRepository.php
+++ Modules/Config/Repositories/ConfigRepository.php
@@ -1,19 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Config\Repositories;
 
 use App\Repositories\BaseRepository;
 use Modules\Config\Models\Config;
 use Modules\Config\Repositories\Criteria\ConfigRequestCriteria;
 
-class ConfigRepository extends BaseRepository
+final class ConfigRepository extends BaseRepository
 {
-    public function model()
+    /**
+     * {@inheritDoc}
+     */
+    final public function model(): string
     {
         return Config::class;
     }
 
-    public function boot()
+    /**
+     * {@inheritDoc}
+     */
+    final public function boot()
     {
         $this->pushCriteria(ConfigRequestCriteria::class);
     }
