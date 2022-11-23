c471548bf834167b8ddc6b75099dace586bffd29
Yeqing <cuiyeqing960904@gmail.com>
2022-11-23 06:17:46 -0500

    routes strct built

--- Modules/Customer/Routes/admin.php
+++ Modules/Customer/Routes/admin.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Customer\Http\Controllers\Admin\CustomerController;
+
+
+Route::group(['middleware' => 'tenant.set:1'], function () {
+    Route::get('/customers/all', [CustomerController::class, 'all'])->name('customers.all');
+    Route::resource('customers', CustomerController::class);
+});
+
