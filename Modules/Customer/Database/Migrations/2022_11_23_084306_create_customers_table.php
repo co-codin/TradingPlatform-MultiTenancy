c15fb3c8f9c654b44c49c19d68c0c7e83e261c0d
Yeqing <cuiyeqing960904@gmail.com>
2022-11-23 05:03:46 -0500

    bulding enums

--- Modules/Customer/Database/Migrations/2022_11_23_084306_create_customers_table.php
+++ Modules/Customer/Database/Migrations/2022_11_23_084306_create_customers_table.php
@@ -15,7 +15,26 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->tinyInteger('gender');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('phone2');
 
+            $table->foreignId('country_id')->constrained();
+
+            $table->foreignId('language_id')->nullable()->constrained();
+            $table->foreignId('supposed_language_id')->nullable()->constrained();
+            $table->foreignId('supposed_language_id')->nullable()->constrained();
+
+            $table->string('city')->nullable();
+            $table->string('address')->nullable();
+            $table->string('postal_code')->nullable();
+
+            $table->tinyInteger('verification_status_id')->nullable();
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
