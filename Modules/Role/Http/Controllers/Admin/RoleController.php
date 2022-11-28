2609845db733929aad936c619a29fb002c1f3235
Denis <denis.a@stoxtech.dev>
2022-11-28 15:22:50 +0300

    STX-254: Описание api endpoints , swagger

--- Modules/Role/Http/Controllers/Admin/RoleController.php
+++ Modules/Role/Http/Controllers/Admin/RoleController.php
@@ -1,37 +1,76 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Role\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
 use Modules\Role\Dto\RoleDto;
 use Modules\Role\Http\Requests\RoleCreateRequest;
-use Modules\Role\Http\Requests\RolePermissionRequest;
 use Modules\Role\Http\Requests\RoleUpdateRequest;
 use Modules\Role\Models\Role;
 use Modules\Role\Repositories\RoleRepository;
 use Modules\Role\Services\RoleStorage;
 use Modules\Role\Http\Resources\RoleResource;
+use Spatie\DataTransferObject\Exceptions\UnknownProperties;
 
-class RoleController extends Controller
+final class RoleController extends Controller
 {
     use ValidatesRequests;
 
-    public function __construct(
+    final public function __construct(
         protected RoleStorage $roleStorage,
         protected RoleRepository $roleRepository
     ) {}
 
-    public function index()
+    /**
+     * @OA\Get(
+     *     path="/admin/roles",
+     *     tags={"Role"},
+     *     security={ {"sanctum": {} }},
+     *     summary="Get roles",
+     *     @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(ref="#/components/schemas/RoleCollection")
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthorized Error"
+     *     ),
+     *     @OA\Response(
+     *          response=403,
+     *          description="Forbidden Error"
+     *     )
+     * )
+     *
+     * Index roles.
+     *
+     * @return AnonymousResourceCollection
+     * @throws AuthorizationException
+     */
+    final public function index(): JsonResource
     {
         $this->authorize('viewAny', Role::class);
 
-        $roles = $this->roleRepository->jsonPaginate();
-
-        return RoleResource::collection($roles);
+        return RoleResource::collection(
+            $this->roleRepository->jsonPaginate()
+        );
     }
 
-    public function show(int $role)
+    /**
+     * Show role.
+     *
+     * @param int $role
+     * @return JsonResource
+     * @throws AuthorizationException
+     */
+    final public function show(int $role): JsonResource
     {
         $role = $this->roleRepository->find($role);
 
@@ -40,36 +79,51 @@ class RoleController extends Controller
         return new RoleResource($role);
     }
 
-    public function store(RoleCreateRequest $request)
+    /**
+     * Store role.
+     *
+     * @param RoleCreateRequest $request
+     * @return JsonResource
+     * @throws AuthorizationException
+     * @throws UnknownProperties
+     */
+    final public function store(RoleCreateRequest $request): JsonResource
     {
         $this->authorize('create', Role::class);
 
-        $role = $this->roleStorage->store(RoleDto::fromFormRequest($request));
-
-        return new RoleResource($role);
+        return new RoleResource(
+            $this->roleStorage->store(RoleDto::fromFormRequest($request))
+        );
     }
 
-    public function update(int $role, RoleUpdateRequest $request)
+    /**
+     * Update role
+     *
+     * @param int $role
+     * @param RoleUpdateRequest $request
+     * @return JsonResource
+     * @throws AuthorizationException
+     * @throws UnknownProperties
+     */
+    final public function update(int $role, RoleUpdateRequest $request): JsonResource
     {
         $role = $this->roleRepository->find($role);
 
         $this->authorize('update', $role);
 
-        $role = $this->roleStorage->update($role, RoleDto::fromFormRequest($request));
-
-        return new RoleResource($role);
+        return new RoleResource(
+            $this->roleStorage->update($role, RoleDto::fromFormRequest($request))
+        );
     }
 
-    public function updatePermissions(RolePermissionRequest $request, int $role)
-    {
-        $role = $this->roleRepository->find($role);
-
-        $this->roleStorage->updatePermissions($role, $request->input('permissions'));
-
-        return response()->noContent();
-    }
-
-    public function destroy(int $role)
+    /**
+     * Destroy role.
+     *
+     * @param int $role
+     * @return Response
+     * @throws AuthorizationException
+     */
+    final public function destroy(int $role): Response
     {
         $role = $this->roleRepository->find($role);
 
--- Modules/Role/Http/Resources/PermissionResource.php
+++ Modules/Role/Http/Resources/PermissionResource.php
@@ -3,15 +3,68 @@
 namespace Modules\Role\Http\Resources;
 
 use App\Http\Resources\BaseJsonResource;
+use Modules\Role\Models\Permission;
 use OpenApi\Annotations as OA;
 
 /**
  * @OA\Schema (
  *     schema="Permission",
- *     type="object"
+ *     type="object",
+ *     required={
+ *         "id",
+ *         "name",
+ *         "guard_name",
+ *         "key",
+ *         "created_at",
+ *         "updated_at",
+ *     },
+ *     @OA\Property(property="id", type="integer", description="Role ID"),
+ *     @OA\Property(property="name", type="string", description="User"),
+ *     @OA\Property(property="guard_name", type="string", description="api"),
+ *     @OA\Property(property="key", type="string", description="key"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", description="Date and time of creation", example="1970-01-01 00:00:00"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", description="Date and time of last update", example="1970-01-01 00:00:00"),
+ *     @OA\Property(property="deleted_at", type="string", format="date-time", nullable=true, description="Date and time of soft delete", example="1970-01-01 00:00:00"),
+ *     @OA\Property(property="permissions", type="array", @OA\Items(ref="#/components/schemas/Permission"), description="Permission array"),
+ * ),
+ *
+ * @OA\Schema (
+ *     schema="PermissionCollection",
+ *     type="object",
+ *     @OA\Property(
+ *         property="data",
+ *         type="array",
+ *         @OA\Items(ref="#/components/schemas/Permission")
+ *     ),
+ *     @OA\Property(
+ *         property="meta",
+ *         type="object",
+ *         ref="#/components/schemas/Meta"
+ *     )
+ * ),
+ *
+ * @OA\Schema (
+ *     schema="PermissionResource",
+ *     type="object",
+ *     @OA\Property(
+ *         property="data",
+ *         type="object",
+ *         ref="#/components/schemas/Permission"
+ *     )
  * )
+ *
+ * Class PermissionResource
+ *
+ * @mixin Permission
  */
 class PermissionResource extends BaseJsonResource
 {
-
+    public function toArray($request): array
+    {
+        return array_merge(parent::toArray($request), [
+            'action' => $this->whenLoaded('action'),
+            'model' => $this->whenLoaded('model'),
+            'columns' => $this->whenLoaded('columns'),
+        ]);
+    }
 }
