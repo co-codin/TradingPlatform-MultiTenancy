1d715e44bcbc0074c5a28372e83f541171bc3618
Tarlank <tarlan.k@stoxtech.dev>
2022-11-30 00:35:49 +0400

    STX-8 Add swagger annotations

--- Modules/Sale/Http/Resources/SaleResource.php
+++ Modules/Sale/Http/Resources/SaleResource.php
@@ -2,17 +2,72 @@
 
 namespace Modules\Sale\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\BaseJsonResource;
+use Modules\Sale\Models\SaleStatus;
+use Illuminate\Http\Request;
 
-class SaleResource extends JsonResource
+/**
+ * Sale.
+ *
+ * @OA\Schema(
+ *     schema="Sale",
+ *     title="Sale",
+ *     description="SaleStatus model",
+ *     required={
+ *         "name",
+ *         "title",
+ *         "color",
+ *     },
+ *     @OA\Xml(name="Sale"),
+ *     @OA\Property(property="id", type="integer", readOnly="true", example="1"),
+ *     @OA\Property(property="name", type="string", example="Any name"),
+ *     @OA\Property(property="title", type="string", example="Any title"),
+ *     @OA\Property(property="is_active", type="boolean", example="1"),
+ *     @OA\Property(property="color", type="string", example="#e1e1e1"),
+ *     @OA\Property(property="created_at", type="string", format="date-time"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time"),
+ *     @OA\Property(property="deleted_at", type="string", format="date-time", nullable=true),
+ * ),
+ *
+ * @OA\Schema (
+ *     schema="SaleCollection",
+ *     type="object",
+ *     @OA\Property(
+ *         property="data",
+ *         type="array",
+ *         @OA\Items(ref="#/components/schemas/Sale")
+ *     ),
+ *     @OA\Property(
+ *         property="meta",
+ *         type="object",
+ *         ref="#/components/schemas/Meta"
+ *     )
+ * ),
+ *
+ * @OA\Schema (
+ *     schema="SaleResource",
+ *     type="object",
+ *     @OA\Property(
+ *         property="data",
+ *         type="object",
+ *         ref="#/components/schemas/Sale"
+ *     )
+ * )
+ *
+ * Class SaleResource
+ *
+ * @package Modules\Sale\Http\Resources
+ * @mixin SaleStatus
+ */
+class SaleResource extends BaseJsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request
+     * @param  Request $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
             'id' => $this->resource->id,
