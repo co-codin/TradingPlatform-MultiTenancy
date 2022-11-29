25f3e2820dc712ff13a5cefcd8c4fb8a492fc425
Tarlank <tarlan.k@stoxtech.dev>
2022-11-29 12:26:47 +0400

    STX-275 Create Sale Migration

---
+++ Modules/Sale/Database/Migrations/2022_11_28_210809_create_sale_statuses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_statuses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('title');
+            $table->boolean('is_active')->default(true);
+            $table->string('color');
+            $table->timestamp('deleted_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_statuses');
+    }
+};
