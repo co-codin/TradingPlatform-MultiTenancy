e8b356e0f2d1ddd8ef52cdfc384342d6c530d064
Tarlank <tarlan.k@stoxtech.dev>
2022-11-29 14:34:36 +0400

    STX-8 Create Sale Request

---
+++ Modules/Sale/Http/Requests/SaleStoreRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Sale\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SaleStoreRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function authorize(): array
+    {
+        return [
+            'name' => 'required|string',
+            'title' => 'required|string',
+            'color' => 'required|string',
+        ];
+    }
+}
