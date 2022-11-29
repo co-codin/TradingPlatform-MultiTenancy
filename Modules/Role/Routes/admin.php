2845845b6c721c51bf199af1dc9308776a9bbe5b
elijah <cuiyeqing960904@gmail.com>
2022-11-29 03:03:11 -0500

    role permission

--- Modules/Role/Routes/admin.php
+++ Modules/Role/Routes/admin.php
@@ -5,6 +5,7 @@ use Modules\Role\Http\Controllers\Admin\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'tenant.set:1'], function () {
+    Route::put('roles/{role}/permissions', [RoleController::class, 'updatePermissions']);
     Route::apiResource('roles', RoleController::class);
 
     Route::get('permissions/all', [PermissionController::class, 'all']);
