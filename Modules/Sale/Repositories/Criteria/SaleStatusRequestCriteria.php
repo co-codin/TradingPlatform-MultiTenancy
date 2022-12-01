e7a08fe27fcf509f64c612430ffe44f2444edf47
Tarlank <tarlan.k@stoxtech.dev>
2022-12-01 17:50:57 +0400

    STX-276: Fix allowedFields (new format)

--- Modules/Sale/Repositories/Criteria/SaleStatusRequestCriteria.php
+++ Modules/Sale/Repositories/Criteria/SaleStatusRequestCriteria.php
@@ -11,53 +11,39 @@ use Spatie\QueryBuilder\QueryBuilder;
 
 final class SaleStatusRequestCriteria extends BaseCriteria
 {
+    /**
+     * {@inheritdoc}
+     */
     protected static array $allowedModelFields = [
         'id',
         'name',
         'title',
     ];
-
     /**
      * {@inheritDoc}
      */
     public function apply($model, RepositoryInterface $repository)
     {
         return QueryBuilder::for($model)
-        ->defaultSort('-id')
-        ->allowedFields(
-            static::allowedSaleFields()
-        )
-        ->allowedFilters([
-            AllowedFilter::exact('id'),
-            AllowedFilter::partial('name'),
-            AllowedFilter::partial('title'),
-            AllowedFilter::partial('created_at'),
-            AllowedFilter::partial('updated_at'),
-            AllowedFilter::trashed(),
-        ])
-        ->allowedSorts([
-            'id',
-            'name',
-            'title',
-            'created_at',
-            'updated_at',
-            'deleted_at',
-        ]);
+            ->defaultSort('-id')
+            ->allowedFields(
+                self::$allowedModelFields,
+            )
+            ->allowedFilters([
+                AllowedFilter::exact('id'),
+                AllowedFilter::partial('name'),
+                AllowedFilter::partial('title'),
+                AllowedFilter::partial('created_at'),
+                AllowedFilter::partial('updated_at'),
+                AllowedFilter::trashed(),
+            ])
+            ->allowedSorts([
+                'id',
+                'name',
+                'title',
+                'created_at',
+                'updated_at',
+                'deleted_at',
+            ]);
     }
-
-    public static function allowedSaleFields($prefix = null): array
-    {
-        $fields = [
-            'id',
-            'name',
-            'title',
-        ];
-
-        if(!$prefix) {
-            return $fields;
-        }
-
-        return array_map(fn($field) => $prefix . "." . $field, $fields);
-    }
-
 }
