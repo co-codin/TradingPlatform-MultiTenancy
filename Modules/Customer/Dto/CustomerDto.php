7e70a4e9b701f7d8ab7181ad69dfad4ba7e72037
Tarlank <tarlan.k@stoxtech.dev>
2022-12-02 00:59:18 +0400

    STX-309: Fields added

--- Modules/Customer/Dto/CustomerDto.php
+++ Modules/Customer/Dto/CustomerDto.php
@@ -27,22 +27,127 @@ final class CustomerDto extends BaseDto
      * @var string
      */
     public string $email = '';
-
     /**
      * @var string
      */
     public string $password = '';
-
     /**
      * @var string
      */
     public string $phone = '';
-
+    /**
+     * @var string
+     */
+    public string $phone2 = '';
     /**
      * @var int
      */
     public int $country_id = 0;
+    /**
+     * @var string
+     */
+    public string $birthday = '';
+    /**
+     * @var int
+     */
+    public int $language_id = 0;
+    /**
+     * @var int
+     */
+    public int $supposed_language_id = 0;
+    /**
+     * @var int
+     */
+    public int $platform_language_id = 0;
+    /**
+     * @var string
+     */
+    public string $state = '';
+    /**
+     * @var string
+     */
+    public string $city = '';
+    /**
+     * @var string
+     */
+    public string $address = '';
+    /**
+     * @var string
+     */
+    public string $postal_code = '';
+    /**
+     * @var string
+     */
+    public bool $is_demo = false;
+    /**
+     * @var bool
+     */
+    public bool $is_active = true;
+    /**
+     * @var bool
+     */
+    public bool $is_active_trading = true;
+    /**
+     * @var bool
+     */
+    public bool $is_test = false;
+    /**
+     * @var int
+     */
+    public int $desk_id = 0;
+    /**
+     * @var int
+     */
+    public int $department_id = 0;
 
+    /**
+     * @var string
+     */
+    public string $offer_name = '';
+    /**
+     * @var string
+     */
+    public string $offer_url = '';
+    /**
+     * @var string
+     */
+    public string $comment_about_customer = '';
+    /**
+     * @var string
+     */
+    public string $source = '';
+    /**
+     * @var string
+     */
+    public string $click_id = '';
+    /**
+     * @var string
+     */
+    public string $free_param_1 = '';
+    /**
+     * @var string
+     */
+    public string $free_param_2 = '';
+    /**
+     * @var string
+     */
+    public string $free_param_3 = '';
+    /**
+     * @var float
+     */
+    public float $balance = 0;
+    /**
+     * @var float
+     */
+    public float $balance_usd = 0;
+    /**
+     * @var bool
+     */
+    public bool $is_ftd = false;
+    /**
+     * @var string
+     */
+    public string $timezone = '';
     /**
      * @var ?string
      */
