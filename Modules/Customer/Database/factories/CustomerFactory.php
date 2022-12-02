3704397886c31d211fd9a322a18042cbd7f7359c
Tarlank <tarlan.k@stoxtech.dev>
2022-12-02 13:55:22 +0400

    STX-309: Customer Factory add fields

--- Modules/Customer/Database/factories/CustomerFactory.php
+++ Modules/Customer/Database/factories/CustomerFactory.php
@@ -5,7 +5,10 @@ namespace Modules\Customer\Database\factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Modules\Customer\Models\Customer;
+use Modules\Department\Models\Department;
+use Modules\Desk\Models\Desk;
 use Modules\Geo\Models\Country;
+use Modules\Language\Models\Language;
 
 class CustomerFactory extends Factory
 {
@@ -31,6 +34,22 @@ class CustomerFactory extends Factory
             'password' => Hash::make('password'),
             'phone' => $this->faker->phoneNumber(),
             'country_id' => $this->faker->randomElement(Country::pluck('id')),
+            'phone2' => $this->faker->phoneNumber(),
+            'language_id' => $this->faker->randomElement(Language::pluck('id')),
+            'city' => $this->faker->city(),
+            'address' => $this->faker->address(),
+            'postal_code' => $this->faker->postcode(),
+            'desk_id' => $this->faker->randomElement(Desk::pluck('id')),
+            'department_id' => $this->faker->randomElement(Department::pluck('id')),
+            'offer_name' => $this->faker->sentence(3),
+            'offer_url' => $this->faker->url(),
+            'comment_about_customer' => $this->faker->sentence(20),
+            'source' => $this->faker->sentence(5),
+            'click_id' => $this->faker->randomDigit(),
+            'free_param_1' => $this->faker->sentence(1),
+            'free_param_2' => $this->faker->sentence(1),
+            'free_param_3' => $this->faker->sentence(1),
+
         ];
     }
 }
