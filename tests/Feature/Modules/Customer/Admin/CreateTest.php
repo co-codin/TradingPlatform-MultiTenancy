4bcba6b17f161ff4ca3505d9507c5da23a18245f
Tarlank <tarlan.k@stoxtech.dev>
2022-12-04 11:46:43 +0400

    STX-309: Add Customer Create test

---
+++ tests/Feature/Modules/Customer/Admin/CreateTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature\Modules\Customer\Admin;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Modules\Customer\Enums\CustomerPermission;
+use Modules\Customer\Models\Customer;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+    use DatabaseTransactions, CustomerAdminTrait;
+    /**
+     * Test authorized user can create customer.
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function authorized_user_can_create_customer(): void
+    {
+        $this->authenticateWithPermission(CustomerPermission::fromValue(CustomerPermission::CREATE_CUSTOMERS));
+
+        $data = Customer::factory()->make()->toArray();
+
+        $response = $this->postJson(route('admin.customers.store'), $data);
+
+        $response->assertCreated();
+
+        $data['password'] = $response['data']['password'];
+
+        $response->assertJson(['data' => $data]);
+    }
+
+    /**
+     * Test authorized user can`t create customer.
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function authorized_user_cant_create_customer(): void
+    {
+        $this->authenticateUser();
+
+        $data = Customer::factory()->make()->toArray();
+
+        $response = $this->postJson(route('admin.customers.store'), $data);
+
+        $response->assertForbidden();
+    }
+
+    /**
+     * Test unauthorized user can`t create customer.
+     *
+     * @return void
+     *
+     * @test
+     */
+    public function unauthorized(): void
+    {
+        $data = Customer::factory()->make()->toArray();
+
+        $response = $this->postJson(route('admin.customers.store'), $data);
+
+        $response->assertUnauthorized();
+    }
+}
