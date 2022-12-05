623c2aaa9a1dd1e5469b9566e7aaee6a11720421
elijah <cuiyeqing960904@gmail.com>
2022-12-05 10:17:07 -0500

    image uploader + image storage built

--- Modules/Media/Services/ImageStorage.php
+++ Modules/Media/Services/ImageStorage.php
@@ -2,7 +2,64 @@
 
 namespace Modules\Media\Services;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Modules\Media\Models\Image;
+
 class ImageStorage
 {
+    protected Model $model;
 
+    protected Collection $images;
+
+    public function update(Model $model, array $images = [])
+    {
+        DB::beginTransaction();
+
+        $this->model = $model;
+        $this->images = collect($images);
+
+        $this
+            ->deleteNonExistentImages()
+            ->createNewImages()
+            ->updateExistingImages();
+
+        DB::commit();
+    }
+
+    protected function deleteNonExistentImages()
+    {
+        $ids = $this->images->pluck('id')->filter()->unique();
+
+        $this->model->images()
+            ->when($ids->isNotEmpty(), fn($query) => $query->whereNotIn('id', $ids))
+            ->delete();
+
+        return $this;
+    }
+
+    protected function createNewImages()
+    {
+        $newImages = $this->images->filter(fn($item) => !Arr::exists($item, 'id'));
+
+        $this->model->images()->createMany($newImages);
+
+        return $this;
+    }
+
+    protected function updateExistingImages()
+    {
+        $this->images
+            ->filter(fn($image) => Arr::exists($image, 'id'))
+            ->each(function($image) {
+                $model = Image::query()->find($image['id']);
+                if($model) {
+                    $model->update($image);
+                }
+            });
+
+        return $this;
+    }
 }
