58282b0d9c921b5c7a96c34e95bdd00cb7ce05c2
Denis <denis.a@stoxtech.dev>
2022-12-06 09:03:19 +0300

    STX-324: fix

--- Modules/Customer/Routes/admin.php
+++ Modules/Customer/Routes/admin.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Modules\Customer\Http\Controllers\Admin\CustomerController;
 
 
-//Route::group(['middleware' => 'tenant.set:1'], function () {
+Route::group(['middleware' => 'tenant.set:1'], function () {
     Route::get('/customers/all', [CustomerController::class, 'all'])->name('customers.all');
     Route::apiResource('customers', CustomerController::class);
-//});
+});
 
