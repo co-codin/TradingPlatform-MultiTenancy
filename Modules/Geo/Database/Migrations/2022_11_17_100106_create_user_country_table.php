7f95b0c7264cb2c3708704bf6ff3c42a08d4cd5f
Tarlank <tarlan.k@stoxtech.dev>
2022-12-07 21:09:21 +0400

    STX-362: Fix public.users

--- Modules/Geo/Database/Migrations/2022_11_17_100106_create_user_country_table.php
+++ Modules/Geo/Database/Migrations/2022_11_17_100106_create_user_country_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('user_country', function (Blueprint $table) {
             $table->primary(['user_id', 'country_id'], 'id');
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->on('public.users');
             $table->foreignId('country_id')->constrained();
         });
     }
