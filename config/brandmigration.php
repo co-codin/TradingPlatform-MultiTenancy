d7411fdf0a034c2a58065bfcb9ac430c0f197eb4
Tarlank <tarlan.k@stoxtech.dev>
2022-12-07 21:12:13 +0400

    STX-362: Add Conf file / REQUIRED_MODULES

---
+++ config/brandmigration.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'REQUIRED_MODULES' => [
+        'Campaign' => 'Campaign',
+        'Config' => 'Config',
+        'Department' => 'Department',
+        'Language' => 'Language',
+        'Geo' => 'Geo',
+        'Desk' => 'Desk',
+        'Sale' => 'Sale',
+        'Customer' => 'Customer',
+    ],
+
+];
