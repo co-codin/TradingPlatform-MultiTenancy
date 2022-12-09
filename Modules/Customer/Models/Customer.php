c4558994a85e9d5fb0f2d84d4656328a33bb0f01
Denis <denis.a@stoxtech.dev>
2022-12-09 13:59:57 +0300

    STX-333: added fixes with tenant brand

--- Modules/Customer/Models/Customer.php
+++ Modules/Customer/Models/Customer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Customer\Models;
 
-use App\Models\Traits\ForTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -16,6 +15,8 @@ use Modules\Customer\Events\CustomerSaving;
 use Modules\Customer\Models\Traits\CustomerRelations;
 use Modules\Geo\Models\Country;
 use Modules\Role\Models\Traits\HasRoles;
+use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
+use Spatie\Multitenancy\Models\Tenant;
 
 /**
  * Class Customer
@@ -48,7 +49,7 @@ final class Customer extends Authenticatable
     use CustomerRelations;
     use HasRoles;
     use HasApiTokens;
-    use ForTenant;
+    use UsesTenantConnection;
 
     /**
      * {@inheritdoc}
@@ -91,6 +92,14 @@ final class Customer extends Authenticatable
     protected $rememberTokenName = false;
 
     /**
+     * {@inheritDoc}
+     */
+    protected static function newFactory(): CustomerFactory
+    {
+        return CustomerFactory::new();
+    }
+
+    /**
      * Country relation.
      *
      * @return BelongsTo
@@ -107,14 +116,8 @@ final class Customer extends Authenticatable
      */
     public function getBrand(): ?Brand
     {
-        return Brand::where('slug', $this->getConnection()->getConfig('search_path'))->first();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected static function newFactory(): CustomerFactory
-    {
-        return CustomerFactory::new();
+        return app(Tenant::class)->current() instanceof Brand ?
+            app(Tenant::class)->current() :
+            null;
     }
 }
