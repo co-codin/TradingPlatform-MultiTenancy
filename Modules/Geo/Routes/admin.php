14a858bc876ae64fb58bae2dbbd3ab7144ceef0e
elijah <cuiyeqing960904@gmail.com>
2022-12-12 11:02:37 -0500

    all methods built

--- Modules/Geo/Routes/admin.php
+++ Modules/Geo/Routes/admin.php
@@ -15,5 +15,6 @@ use Modules\Geo\Http\Controllers\Admin\CountryController;
 */
 
 Route::group(['middleware' => 'tenant'], function () {
+    Route::get('countries/all', [CountryController::class, 'all']);
     Route::apiResource('countries', CountryController::class);
 });
--- Modules/Language/Routes/admin.php
+++ Modules/Language/Routes/admin.php
@@ -17,5 +17,6 @@ use Modules\Language\Http\Controllers\Admin\LanguageController;
 */
 
 Route::group(['middleware' => 'tenant'], function () {
+    Route::get('languages/all', [LanguageController::class, 'all']);
     Route::apiResource('languages', LanguageController::class);
 });
