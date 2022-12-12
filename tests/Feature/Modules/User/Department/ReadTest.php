bd516d62063f8b1fd872b9bc4f1c6e7f3f72010b
elijah <cuiyeqing960904@gmail.com>
2022-12-12 09:22:06 -0500

    logics

--- tests/Feature/Modules/User/Department/ReadTest.php
+++ tests/Feature/Modules/User/Department/ReadTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Tests\Feature\Modules\User\Department;
 
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Modules\Department\Models\Department;
+use Modules\Role\Models\Permission;
 use Modules\User\Enums\UserPermission;
 use Spatie\Multitenancy\Commands\Concerns\TenantAware;
 use Tests\BrandTestCase;
@@ -19,16 +21,16 @@ class ReadTest extends BrandTestCase
 
     public function test_authorized_user_can_read_users_with_departments()
     {
+        $route = '/admin/users?include=departments';
+
         $this->authenticateWithPermission(UserPermission::fromValue(UserPermission::VIEW_USERS));
 
         $this->brand->makeCurrent();
 
         $department = Department::factory()->create();
 
-        $response = $this->getJson(route('admin.users.index'));
 
-        dd(
-            $response->json()
-        );
+        $response = $this->getJson($route);
+        
     }
 }
