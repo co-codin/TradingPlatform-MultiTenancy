fc2bfbe738ece8e8201c6fbb8a7b04a1efbe6981
Tarlank <tarlan.k@stoxtech.dev>
2022-12-14 15:16:10 +0400

    STX-150: Add UserImpersonateController

---
+++ Modules/User/Http/Controllers/Admin/Impersonate/UserImpersonateController.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Http\Controllers\Admin\Impersonate;
+
+use App\Http\Controllers\Controller;
+use Carbon\CarbonImmutable;
+use Modules\User\Http\Resources\AuthUserResource;
+use Modules\User\Repositories\UserRepository;
+
+class UserImpersonateController extends Controller
+{
+    public function __construct(
+        protected UserRepository $userRepository
+    ) {
+    }
+
+    public function update(int $id)
+    {
+        $impersonator = auth()->user();
+
+        $this->authorize('update', $impersonator);
+
+        $newWorker = $this->userRepository->find($id);
+
+        $expiredAt = CarbonImmutable::now()->add(config('auth.api_token_expires_in'));
+
+        $newWorkerToken = $newWorker->createToken('api', expiresAt: $expiredAt);
+
+        return response()->json([
+            'data' => [
+                'requested_worker' => new AuthUserResource($impersonator),
+                'new_worker' => new AuthUserResource($newWorker),
+                'new_token' => $newWorkerToken->plainTextToken,
+            ],
+        ]);
+    }
+}
