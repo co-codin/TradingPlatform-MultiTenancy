83f5f821b303b58f5dcbfd37fcfaff76628982ed
Tarlank <tarlan.k@stoxtech.dev>
2022-12-15 23:22:50 +0400

    STX-369: Add makeCurrent

--- tests/Feature/Modules/Customer/ExportTest.php
+++ tests/Feature/Modules/Customer/ExportTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Feature\Modules\Customer\Admin;
 
 use Modules\Customer\Enums\CustomerPermission;
+use Spatie\Multitenancy\Commands\Concerns\TenantAware;
 use Tests\BrandTestCase;
 use Tests\Traits\HasAuth;
 
 final class ExportTest extends BrandTestCase
 {
+    use TenantAware;
     use HasAuth;
 
     /**
@@ -18,6 +20,8 @@ final class ExportTest extends BrandTestCase
     public function test_export_excel_customers(): void
     {
         $this->authenticateWithPermission(CustomerPermission::fromValue(CustomerPermission::EXPORT_CUSTOMERS));
+
+        $this->brand->makeCurrent();
 
         $response = $this->post(route('admin.customers.export.excel'));
 
@@ -30,6 +34,8 @@ final class ExportTest extends BrandTestCase
     public function test_export_csv_customers(): void
     {
         $this->authenticateWithPermission(CustomerPermission::fromValue(CustomerPermission::EXPORT_CUSTOMERS));
+
+        $this->brand->makeCurrent();
 
         $response = $this->post(route('admin.customers.export.csv'));
 
