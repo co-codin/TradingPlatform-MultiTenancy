c7672dd5a06eee2d74c624f85a0d91590f0bfd35
Tarlank <tarlan.k@stoxtech.dev>
2022-12-19 12:18:53 +0400

    STX-423: Add emails route

--- Modules/Communication/Routes/admin.php
+++ Modules/Communication/Routes/admin.php
@@ -5,4 +5,5 @@ use Modules\Communication\Http\Controllers\Admin\CommentController;
 
 Route::group(['middleware' => 'tenant'], function () {
     Route::apiResource('comments', CommentController::class);
+    Route::apiResource('emails', EmailController::class);
 });
