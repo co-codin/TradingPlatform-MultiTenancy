bc8221184e9ea92546c06bbd3fa99581045fec70
Tarlank <tarlan.k@stoxtech.dev>
2022-12-19 15:25:19 +0400

    STX-423: Create EmailUpdateRequest

--- Modules/Communication/Http/Requests/EmailUpdateRequest.php
+++ Modules/Communication/Http/Requests/EmailUpdateRequest.php
@@ -8,5 +8,19 @@ use App\Http\Requests\BaseFormRequest;
 
 final class EmailUpdateRequest extends BaseFormRequest
 {
-
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'email_template_id' => 'required|int|exists:tenant.email_templates,id',
+            'subject' => 'required|string',
+            'body' => 'required|string',
+            'sent_by_system' => 'sometimes|boolean',
+            'user_id' => 'sometimes|int|exists:users,id',
+        ];
+    }
 }
