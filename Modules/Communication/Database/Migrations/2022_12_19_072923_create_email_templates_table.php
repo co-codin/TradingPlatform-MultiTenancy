3f91a73b98b18abeedd534fc27648d1f9e398aaf
Tarlank <tarlan.k@stoxtech.dev>
2022-12-19 14:27:25 +0400

    STX-423: Update email templates migration

--- Modules/Communication/Database/Migrations/2022_12_19_072923_create_email_templates_table.php
+++ Modules/Communication/Database/Migrations/2022_12_19_072923_create_email_templates_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('email_templates', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('body');
             $table->timestamps();
         });
     }
