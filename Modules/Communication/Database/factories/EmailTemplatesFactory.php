812cbd0e3085ba0605ef64158de8282e191c0385
Tarlank <tarlan.k@stoxtech.dev>
2022-12-19 14:26:59 +0400

    STX-423: Add EmailTemplatesFactory

---
+++ Modules/Communication/Database/factories/EmailTemplatesFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Communication\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Communication\Models\EmailTemplates;
+
+class EmailTemplatesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = EmailTemplates::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(3),
+            'body' => $this->faker->sentence(10),
+        ];
+    }
+}
