e45b02b485ca1e95a77aa39f0f8a81c0db16e3e1
Tarlank <tarlan.k@stoxtech.dev>
2022-12-19 13:16:58 +0400

    STX-423: Create EmailCreateRequest

---
+++ Modules/Communication/Http/Requests/EmailCreateRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Communication\Http\Requests;
+
+use App\Http\Requests\BaseFormRequest;
+
+final class EmailCreateRequest extends BaseFormRequest
+{
+
+}
