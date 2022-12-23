65008d4672bab2547a2943c337f3f9a223231dfe
Denis <denis.a@stoxtech.dev>
2022-12-23 09:38:38 +0300

    STX-451: fix

--- Modules/Transaction/Http/Requests/TransactionStatusCreateRequest.php
+++ Modules/Transaction/Http/Requests/TransactionStatusCreateRequest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Modules\Transaction\Http\Requests;
 
 use App\Http\Requests\BaseFormRequest;
+use BenSampo\Enum\Rules\EnumValue;
+use Modules\Transaction\Enums\TransactionStatusName;
 
 final class TransactionStatusCreateRequest extends BaseFormRequest
 {
@@ -14,7 +16,12 @@ final class TransactionStatusCreateRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                new EnumValue(TransactionStatusName::class, false),
+            ],
             'title' => 'required|string|max:255',
             'is_active' => 'sometimes|required|boolean|max:255',
             'is_valid' => 'sometimes|required|boolean|max:255',
