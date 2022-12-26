97d29c0cf2737a130ee0bbe8bf1efacb6b808c8d
Tarlank <tarlan.k@stoxtech.dev>
2022-12-26 14:13:10 +0400

    STX-453: Final tests

--- Modules/Transaction/Providers/TransactionServiceProvider.php
+++ Modules/Transaction/Providers/TransactionServiceProvider.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Modules\Transaction\Providers;
 
 use App\Providers\BaseModuleServiceProvider;
+use Modules\Transaction\Models\TransactionsMt5Type;
 use Modules\Transaction\Models\TransactionStatus;
+use Modules\Transaction\Policies\TransactionsMt5TypePolicy;
 use Modules\Transaction\Policies\TransactionStatusPolicy;
 
 final class TransactionServiceProvider extends BaseModuleServiceProvider
@@ -15,6 +17,7 @@ final class TransactionServiceProvider extends BaseModuleServiceProvider
      */
     protected array $policies = [
         TransactionStatus::class => TransactionStatusPolicy::class,
+        TransactionsMt5Type::class => TransactionsMt5TypePolicy::class,
     ];
 
     /**
