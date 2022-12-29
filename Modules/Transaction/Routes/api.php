04f4cdd265ec8014aab9b465ae0e1ef191df8fa2
Denis <denis.a@stoxtech.dev>
2022-12-29 12:04:34 +0300

    STX-477: fix

--- Modules/Transaction/Routes/api.php
+++ Modules/Transaction/Routes/api.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Modules\Transaction\Http\Controllers\TransactionController;
 
-Route::group(['middleware' => 'auth:web-customer'], function () {
+Route::group(['middleware' => 'auth:api-customer'], function () {
     Route::apiResource('transactions', TransactionController::class);
 });
