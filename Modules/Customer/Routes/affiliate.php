2014095a1d563720dfcc51f9d50ec01b11a88deb
Tarlank <tarlan.k@stoxtech.dev>
2023-01-11 13:44:09 +0400

    STX-499: Fix

--- Modules/Customer/Routes/affiliate.php
+++ Modules/Customer/Routes/affiliate.php
@@ -9,7 +9,7 @@ Route::group(['middleware' => 'affiliation-token'], function () {
     Route::post('auth/register', [RegisterController::class, 'register']);
 
     Route::group(['middleware' => 'auth:api'], function () {
-        Route::apiResource('customers', CustomerController::class)->only(['index']);
+        Route::apiResource('customers', CustomerController::class)->only(['index', 'store']);
         Route::apiResource('ftd-customers', FTDCustomerController::class)->only(['index']);
     });
 });
