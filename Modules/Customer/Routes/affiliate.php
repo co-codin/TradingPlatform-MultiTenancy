00caddca08a34226e7018571c89c6ee3b9e5923f
elijah <elijah@elijahs-MacBook-Pro.local>
2023-01-12 12:29:56 +0300

    updated

--- Modules/Customer/Routes/affiliate.php
+++ Modules/Customer/Routes/affiliate.php
@@ -5,9 +5,6 @@ use Modules\Customer\Http\Controllers\Affiliate\CustomerController;
 use Modules\Customer\Http\Controllers\Affiliate\FTDCustomerController;
 
 Route::group(['middleware' => 'affiliation-token'], function () {
-
-    Route::group([], function () {
-        Route::apiResource('customers', CustomerController::class)->only(['index', 'store']);
-        Route::apiResource('ftd-customers', FTDCustomerController::class)->only(['index']);
-    });
+    Route::apiResource('customers', CustomerController::class)->only(['index', 'store']);
+    Route::apiResource('ftd-customers', FTDCustomerController::class)->only(['index']);
 });
