f9eb32460fe928b46d91e9f5e9e44ac821e2605d
Denis <denis.a@stoxtech.dev>
2023-02-06 13:17:17 +0300

    STX-659: fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Modules\Brand\Database\Seeders\BrandDatabaseSeeder;
-use Modules\Brand\Models\Brand;
 use Modules\Campaign\Database\Seeders\CampaignDatabaseSeeder;
 use Modules\Currency\Database\Seeders\CurrencyDatabaseSeeder;
 use Modules\Geo\Database\Seeders\GeoDatabaseSeeder;
@@ -15,8 +14,6 @@ use Modules\Role\Database\Seeders\RoleDatabaseSeeder;
 use Modules\Role\Database\Seeders\RolePermissionColumnsSeeder;
 use Modules\Splitter\Database\Seeders\SplitterDatabaseSeeder;
 use Modules\User\Database\Seeders\UserDatabaseSeeder;
-use Modules\User\Models\User;
-use Modules\User\Models\WorkerInfo;
 
 final class DatabaseSeeder extends Seeder
 {
@@ -27,12 +24,6 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $brand = Brand::first()->makeCurrent();
-        $user = User::factory()->create();
-        $user->workerInfo()->updateOrCreate([], WorkerInfo::factory()->raw());
-        dump($user->workerInfo->first_name);
-        $user->workerInfo()->updateOrCreate([], WorkerInfo::factory()->raw());
-        dd($user->workerInfo);
         $this->call([
             CurrencyDatabaseSeeder::class,
             LanguageDatabaseSeeder::class,
